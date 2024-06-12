3ea98d61e2105bdf3a1d3237378563c9e4bbd928
taalaibek1 <87862312+taalaibek1@users.noreply.github.com>
2024-06-12 14:30:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
         BankAccount bankAccount = new BankAccount(15000);
         try {
             while (true) {
                 bankAccount.withDraw(6000);
-                System.out.println("Remnant in bank accoun:" + bankAccount.getAmount());
+                System.out.println("Remnant in bank account: " + bankAccount.getAmount());
             }
-            }catch (LimitException e){
-            System.out.println("Impossible to withdraw. " + e.getMessage() + " Available sums are: " + e.getRemainingAmount());
+        } catch (LimitException e) {
+            System.out.println("Impossible to withdraw. " + e.getMessage() + " Available amount: " + e.getRemainingAmount());
+            bankAccount.withDraw((int) bankAccount.getAmount());
+            System.out.println("Remnant in bank account: " + bankAccount.getAmount());
         }
     }
 }
